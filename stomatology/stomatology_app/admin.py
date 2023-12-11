481db322154e7f68e015e621c3dad87d6d6a7803
Zhanibek Alibekov <zhanibek.alibekov@effem.com>
2023-12-11 19:57:58 +0300

    change admin.py

--- stomatology/stomatology_app/admin.py
+++ stomatology/stomatology_app/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Doctor)
-admin.site.register(Patient)
-admin.site.register(Session)
-admin.site.register(Worklist)
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname']
 
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname']
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['doctor', 'patient', 'date_time']
+
+@admin.register(Worklist)
+class WorklistAdmin(admin.ModelAdmin):
+    list_display = ['session', 'name']
 
